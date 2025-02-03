d2e07256b350993b7657da10bb1dcd3e0a8b1e35
srxrt <str9703@gmail.com>
2025-02-03 18:53:03 +0900

    fix: modify write article component

--- libs/components/community/Teditor.tsx
+++ libs/components/community/Teditor.tsx
@@ -1,31 +1,42 @@
-import React, { useMemo, useRef, useState } from 'react';
-import { Box, Button, FormControl, MenuItem, Stack, Typography, Select, TextField } from '@mui/material';
-import { BoardArticleCategory } from '../../enums/board-article.enum';
-import { Editor } from '@toast-ui/react-editor';
-import { getJwtToken } from '../../auth';
-import { REACT_APP_API_URL } from '../../config';
-import { useRouter } from 'next/router';
-import axios from 'axios';
-import { T } from '../../types/common';
-import '@toast-ui/editor/dist/toastui-editor.css';
-import { useMutation } from '@apollo/client';
-import { CREATE_BOARD_ARTICLE } from '../../../apollo/user/mutation';
-import { sweetErrorHandling, sweetTopSuccessAlert } from '../../sweetAlert';
-import { Message } from '../../enums/common.enum';
+import React, { useMemo, useRef, useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  MenuItem,
+  Stack,
+  Typography,
+  Select,
+  TextField,
+} from "@mui/material";
+import { BoardArticleCategory } from "../../enums/board-article.enum";
+import { Editor } from "@toast-ui/react-editor";
+import { getJwtToken } from "../../auth";
+import { REACT_APP_API_URL } from "../../config";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { T } from "../../types/common";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { useMutation } from "@apollo/client";
+import { CREATE_BOARD_ARTICLE } from "../../../apollo/user/mutation";
+import { sweetErrorHandling, sweetTopSuccessAlert } from "../../sweetAlert";
+import { Message } from "../../enums/common.enum";
 
 const TuiEditor = () => {
   const editorRef = useRef<Editor>(null),
     token = getJwtToken(),
     router = useRouter();
-  const [articleCategory, setArticleCategory] = useState<BoardArticleCategory>(BoardArticleCategory.FREE);
+  const [articleCategory, setArticleCategory] = useState<BoardArticleCategory>(
+    BoardArticleCategory.FREE
+  );
 
   /** APOLLO REQUESTS **/
   const [createBoardArticle] = useMutation(CREATE_BOARD_ARTICLE);
 
   const memoizedValues = useMemo(() => {
-    const articleTitle = '',
-      articleContent = '',
-      articleImage = '';
+    const articleTitle = "",
+      articleContent = "",
+      articleImage = "";
 
     return { articleTitle, articleContent, articleImage };
   }, []);
@@ -35,40 +46,44 @@ const TuiEditor = () => {
     try {
       const formData = new FormData();
       formData.append(
-        'operations',
+        "operations",
         JSON.stringify({
           query: `mutation ImageUploader($file: Upload!, $target: String!) {
 						imageUploader(file: $file, target: $target) 
 				  }`,
           variables: {
             file: null,
-            target: 'article',
+            target: "article",
           },
-        }),
+        })
       );
       formData.append(
-        'map',
+        "map",
         JSON.stringify({
-          '0': ['variables.file'],
-        }),
+          "0": ["variables.file"],
+        })
       );
-      formData.append('0', image);
+      formData.append("0", image);
 
-      const response = await axios.post(`${process.env.REACT_APP_API_GRAPHQL_URL}`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          'apollo-require-preflight': true,
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_GRAPHQL_URL}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            "apollo-require-preflight": true,
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
       const responseImage = response.data.data.imageUploader;
-      console.log('=responseImage: ', responseImage);
+      console.log("=responseImage: ", responseImage);
       memoizedValues.articleImage = responseImage;
 
       return `${REACT_APP_API_URL}/${responseImage}`;
     } catch (err) {
-      console.log('Error, uploadImage:', err);
+      console.log("Error, uploadImage:", err);
     }
   };
 
@@ -87,7 +102,10 @@ const TuiEditor = () => {
       const articleContent = editor?.getInstance().getHTML() as string;
       memoizedValues.articleContent = articleContent;
 
-      if (memoizedValues.articleContent === '' && memoizedValues.articleTitle === '') {
+      if (
+        memoizedValues.articleContent === "" &&
+        memoizedValues.articleTitle === ""
+      ) {
         throw new Error(Message.INSERT_ALL_INPUTS);
       }
 
@@ -97,11 +115,11 @@ const TuiEditor = () => {
         },
       });
 
-      await sweetTopSuccessAlert('Article is created successfully', 700);
+      await sweetTopSuccessAlert("Article is created successfully", 700);
       await router.push({
-        pathname: '/mypage',
+        pathname: "/mypage",
         query: {
-          category: 'myArticles',
+          category: "myArticles",
         },
       });
     } catch (err: any) {
@@ -111,58 +129,179 @@ const TuiEditor = () => {
   };
 
   const doDisabledCheck = () => {
-    if (memoizedValues.articleContent === '' || memoizedValues.articleTitle === '') {
+    if (
+      memoizedValues.articleContent === "" ||
+      memoizedValues.articleTitle === ""
+    ) {
       return true;
     }
   };
 
   return (
     <Stack>
-      <Stack direction="row" style={{ margin: '40px' }} justifyContent="space-evenly">
-        <Box component={'div'} className={'form_row'} style={{ width: '300px' }}>
-          <Typography style={{ color: '#7f838d', margin: '10px' }} variant="h3">
+      <Stack
+        direction="row"
+        style={{ margin: "40px" }}
+        justifyContent="space-evenly"
+      >
+        <Box
+          component={"div"}
+          className={"form_row"}
+          style={{ width: "300px" }}
+        >
+          <Typography style={{ color: "#999999", margin: "10px" }} variant="h3">
             Category
           </Typography>
-          <FormControl sx={{ width: '100%', background: 'white' }}>
+          <FormControl sx={{ width: "100%", background: "#141414" }}>
             <Select
               value={articleCategory}
               onChange={changeCategoryHandler}
               displayEmpty
-              inputProps={{ 'aria-label': 'Without label' }}
+              sx={{
+                backgroundColor: "#141414", // Background of the Select field
+                color: "white", // Text color
+                ".MuiOutlinedInput-notchedOutline": {
+                  borderColor: "#262626",
+                },
+                "&:hover .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "#262626",
+                },
+                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "#262626",
+                },
+                ".MuiSvgIcon-root": {
+                  color: "white", // Dropdown arrow color
+                },
+              }}
+              inputProps={{ "aria-label": "Without label" }}
+              MenuProps={{
+                PaperProps: {
+                  sx: {
+                    backgroundColor: "#141414", // Menu background
+                  },
+                },
+              }}
             >
-              <MenuItem value={BoardArticleCategory.FREE}>
+              <MenuItem
+                value={BoardArticleCategory.FREE}
+                sx={{
+                  backgroundColor: "#141414",
+                  color: "white",
+                  "&:hover": { backgroundColor: "#1a1a1a" },
+                  "&.Mui-selected": { backgroundColor: "#1a1a1a !important" },
+                  "&.Mui-selected:hover": { backgroundColor: "#1a1a1a" },
+                }}
+              >
                 <span>Free</span>
               </MenuItem>
-              <MenuItem value={BoardArticleCategory.HUMOR}>Humor</MenuItem>
-              <MenuItem value={BoardArticleCategory.NEWS}>News</MenuItem>
-              <MenuItem value={BoardArticleCategory.RECOMMEND}>Recommendation</MenuItem>
+              <MenuItem
+                sx={{
+                  backgroundColor: "#141414",
+                  color: "white",
+                  "&:hover": { backgroundColor: "#1a1a1a" },
+                  "&.Mui-selected": { backgroundColor: "#1a1a1a !important" },
+                  "&.Mui-selected:hover": { backgroundColor: "#1a1a1a" },
+                }}
+                value={BoardArticleCategory.HUMOR}
+              >
+                Humor
+              </MenuItem>
+              <MenuItem
+                value={BoardArticleCategory.NEWS}
+                sx={{
+                  backgroundColor: "#141414",
+                  color: "white",
+                  "&:hover": { backgroundColor: "#1a1a1a" },
+                  "&.Mui-selected": { backgroundColor: "#1a1a1a !important" },
+                  "&.Mui-selected:hover": { backgroundColor: "#1a1a1a" },
+                }}
+              >
+                News
+              </MenuItem>
+              <MenuItem
+                value={BoardArticleCategory.RECOMMEND}
+                sx={{
+                  backgroundColor: "#141414",
+                  color: "white",
+                  "&:hover": { backgroundColor: "#1a1a1a" },
+                  "&.Mui-selected": { backgroundColor: "#1a1a1a !important" },
+                  "&.Mui-selected:hover": { backgroundColor: "#1a1a1a" },
+                }}
+              >
+                Recommendation
+              </MenuItem>
             </Select>
           </FormControl>
         </Box>
-        <Box component={'div'} style={{ width: '300px', flexDirection: 'column' }}>
-          <Typography style={{ color: '#7f838d', margin: '10px' }} variant="h3">
+        <Box
+          component={"div"}
+          style={{ width: "300px", flexDirection: "column" }}
+        >
+          <Typography style={{ color: "#999999", margin: "10px" }} variant="h3">
             Title
           </Typography>
           <TextField
             onChange={articleTitleHandler}
             id="filled-basic"
-            label="Type Title"
-            style={{ width: '300px', background: 'white' }}
+            InputProps={{
+              disableUnderline: true, // Removes the underline
+              sx: {
+                outline: "none",
+                border: "none",
+
+                backgroundColor: "#141414", // Background color
+                color: "white", // Text color
+                padding: "10px", // Adds padding
+              },
+            }}
+            sx={{
+              backgroundColor: "#141414",
+              color: "white",
+              borderRadius: "4px",
+              "& .MuiFilledInput-root": {
+                backgroundColor: "#141414", // Background color
+                boxShadow: "none", // Remove shadow
+                border: "1px solid #262626 ", // Remove border
+              },
+              "& .MuiFilledInput-root:hover": {
+                backgroundColor: "#1a1a1a", // Slight hover effect
+              },
+              "& .MuiFilledInput-root:before, & .MuiFilledInput-root:after": {
+                content: "none", // Removes the default focus outline
+              },
+              "& .MuiInputBase-input": {
+                color: "white", // Text color
+              },
+              "& .Mui-focused": {
+                backgroundColor: "#141414 !important", // Ensures no background change on focus
+                boxShadow: "none !important", // Remove focus shadow
+                outline: "none !important", // Remove focus outline
+                border: "1px solid #262626 ", // Ensure no border
+              },
+              "& .MuiOutlinedInput-notchedOutline": {
+                border: "1px solid #262626 ", // Removes the default border
+              },
+              "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+              {
+                border: "1px solid #262626 ", // Remove focus border
+              },
+            }}
           />
         </Box>
       </Stack>
 
       <Editor
-        initialValue={'Type here'}
-        placeholder={'Type here'}
-        previewStyle={'vertical'}
-        height={'640px'}
+        theme="dark"
+        initialValue={"Type here"}
+        placeholder={"Type here"}
+        previewStyle={"vertical"}
+        height={"640px"}
         // @ts-ignore
-        initialEditType={'WYSIWYG'}
+        initialEditType={"WYSIWYG"}
         toolbarItems={[
-          ['heading', 'bold', 'italic', 'strike'],
-          ['image', 'table', 'link'],
-          ['ul', 'ol', 'task'],
+          ["heading", "bold", "italic", "strike"],
+          ["image", "table", "link"],
+          ["ul", "ol", "task"],
         ]}
         ref={editorRef}
         hooks={{
@@ -180,8 +319,13 @@ const TuiEditor = () => {
       <Stack direction="row" justifyContent="center">
         <Button
           variant="contained"
-          color="primary"
-          style={{ margin: '30px', width: '250px', height: '45px' }}
+          style={{
+            margin: "30px",
+            width: "250px",
+            height: "45px",
+            backgroundColor: "#703fb7",
+            color: "#ffffff",
+          }}
           onClick={handleRegisterButton}
         >
           Register
