ddab24bfacde18cf65a9e292cc15a85854dcc739
srxrt <str9703@gmail.com>
2025-02-03 14:22:34 +0900

    feat: develop add property component

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,30 +1,38 @@
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { NextPage } from 'next';
-import { Stack } from '@mui/material';
-import useDeviceDetect from '../../libs/hooks/useDeviceDetect';
-import withLayoutBasic from '../../libs/components/layout/LayoutBasic';
-import MyProperties from '../../libs/components/mypage/MyProperties';
-import MyFavorites from '../../libs/components/mypage/MyFavorites';
-import RecentlyVisited from '../../libs/components/mypage/RecentlyVisited';
-import AddProperty from '../../libs/components/mypage/AddNewProperty';
-import MyProfile from '../../libs/components/mypage/MyProfile';
-import MyArticles from '../../libs/components/mypage/MyArticles';
-import { useMutation, useReactiveVar } from '@apollo/client';
-import { userVar } from '../../apollo/store';
-import MyMenu from '../../libs/components/mypage/MyMenu';
-import WriteArticle from '../../libs/components/mypage/WriteArticle';
-import MemberFollowers from '../../libs/components/member/MemberFollowers';
-import { sweetErrorHandling, sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
-import MemberFollowings from '../../libs/components/member/MemberFollowings';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { LIKE_TARGET_MEMBER, LIKE_TARGET_PROPERTY, SUBSCRIBE, UNSUBSCRIBE } from '../../apollo/user/mutation';
-import { Message } from '../../libs/enums/common.enum';
-import { Messages } from '../../libs/config';
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { NextPage } from "next";
+import { Stack } from "@mui/material";
+import useDeviceDetect from "../../libs/hooks/useDeviceDetect";
+import withLayoutBasic from "../../libs/components/layout/LayoutBasic";
+import MyProperties from "../../libs/components/mypage/MyProperties";
+import MyFavorites from "../../libs/components/mypage/MyFavorites";
+import RecentlyVisited from "../../libs/components/mypage/RecentlyVisited";
+import AddProperty from "../../libs/components/mypage/AddNewProperty";
+import MyProfile from "../../libs/components/mypage/MyProfile";
+import MyArticles from "../../libs/components/mypage/MyArticles";
+import { useMutation, useReactiveVar } from "@apollo/client";
+import { userVar } from "../../apollo/store";
+import MyMenu from "../../libs/components/mypage/MyMenu";
+import WriteArticle from "../../libs/components/mypage/WriteArticle";
+import MemberFollowers from "../../libs/components/member/MemberFollowers";
+import {
+  sweetErrorHandling,
+  sweetMixinErrorAlert,
+  sweetTopSmallSuccessAlert,
+} from "../../libs/sweetAlert";
+import MemberFollowings from "../../libs/components/member/MemberFollowings";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import {
+  LIKE_TARGET_MEMBER,
+  LIKE_TARGET_PROPERTY,
+  SUBSCRIBE,
+  UNSUBSCRIBE,
+} from "../../apollo/user/mutation";
+import { Messages } from "../../libs/config";
 
 export const getStaticProps = async ({ locale }: any) => ({
   props: {
-    ...(await serverSideTranslations(locale, ['common'])),
+    ...(await serverSideTranslations(locale, ["common"])),
   },
 });
 
@@ -32,7 +40,7 @@ const MyPage: NextPage = () => {
   const device = useDeviceDetect();
   const user = useReactiveVar(userVar);
   const router = useRouter();
-  const category: any = router.query?.category ?? 'myProfile';
+  const category: any = router.query?.category ?? "myProfile";
 
   /** APOLLO REQUESTS **/
   const [subscribe] = useMutation(SUBSCRIBE);
@@ -41,7 +49,7 @@ const MyPage: NextPage = () => {
 
   /** LIFECYCLES **/
   useEffect(() => {
-    if (!user._id) router.push('/').then();
+    if (!user._id) router.push("/").then();
   }, [user]);
 
   /** HANDLERS **/
@@ -54,7 +62,7 @@ const MyPage: NextPage = () => {
           input: id,
         },
       });
-      await sweetTopSmallSuccessAlert('Subscribed!', 800);
+      await sweetTopSmallSuccessAlert("Subscribed!", 800);
       await refetch({ input: query });
     } catch (err: any) {
       sweetErrorHandling(err).then();
@@ -70,7 +78,7 @@ const MyPage: NextPage = () => {
           input: id,
         },
       });
-      await sweetTopSmallSuccessAlert('Unsubscribed!', 800);
+      await sweetTopSmallSuccessAlert("Unsubscribed!", 800);
       await refetch({ input: query });
     } catch (err: any) {
       sweetErrorHandling(err).then();
@@ -86,44 +94,45 @@ const MyPage: NextPage = () => {
           input: id,
         },
       });
-      await sweetTopSmallSuccessAlert('Success!', 800);
+      await sweetTopSmallSuccessAlert("Success!", 800);
       await refetch({ input: query });
     } catch (err: any) {
-      console.log('ERROR, likeMemberHandler', err.message);
+      console.log("ERROR, likeMemberHandler", err.message);
       sweetMixinErrorAlert(err.message).then();
     }
   };
 
   const redirectToMemberPageHandler = async (memberId: string) => {
     try {
-      if (memberId === user?._id) await router.push(`/mypage?memberId=${memberId}`);
+      if (memberId === user?._id)
+        await router.push(`/mypage?memberId=${memberId}`);
       else await router.push(`/member?memberId=${memberId}`);
     } catch (error) {
       await sweetErrorHandling(error);
     }
   };
 
-  if (device === 'mobile') {
+  if (device === "mobile") {
     return <div>MY PAGE</div>;
   } else {
     return (
-      <div id="my-page" style={{ position: 'relative' }}>
+      <div id="my-page" style={{ position: "relative" }}>
         <div className="container">
-          <Stack className={'my-page'}>
-            <Stack className={'back-frame'}>
-              <Stack className={'left-config'}>
+          <Stack className={"my-page"}>
+            <Stack className={"back-frame"}>
+              <Stack className={"left-config"}>
                 <MyMenu />
               </Stack>
-              <Stack className="main-config" mb={'76px'}>
-                <Stack className={'list-config'}>
-                  {category === 'addProperty' && <AddProperty />}
-                  {category === 'myProperties' && <MyProperties />}
-                  {category === 'myFavorites' && <MyFavorites />}
-                  {category === 'recentlyVisited' && <RecentlyVisited />}
-                  {category === 'myArticles' && <MyArticles />}
-                  {category === 'writeArticle' && <WriteArticle />}
-                  {category === 'myProfile' && <MyProfile />}
-                  {category === 'followers' && (
+              <Stack className="main-config" mb={"76px"}>
+                <Stack className={"list-config"}>
+                  {category === "addProperty" && <AddProperty />}
+                  {category === "myProperties" && <MyProperties />}
+                  {category === "myFavorites" && <MyFavorites />}
+                  {category === "recentlyVisited" && <RecentlyVisited />}
+                  {category === "myArticles" && <MyArticles />}
+                  {category === "writeArticle" && <WriteArticle />}
+                  {category === "myProfile" && <MyProfile />}
+                  {category === "followers" && (
                     <MemberFollowers
                       subscribeHandler={subscribeHandler}
                       unsubscribeHandler={unsubscribeHandler}
@@ -131,7 +140,7 @@ const MyPage: NextPage = () => {
                       likeMemberHandler={likeMemberHandler}
                     />
                   )}
-                  {category === 'followings' && (
+                  {category === "followings" && (
                     <MemberFollowings
                       subscribeHandler={subscribeHandler}
                       unsubscribeHandler={unsubscribeHandler}
