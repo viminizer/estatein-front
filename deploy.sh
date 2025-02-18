56707543c8d7f4b18994bd4d749ce399c72bf9f9
srxrt <str9703@gmail.com>
2025-02-19 00:18:08 +0900

    fix: modified deployshell

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ git reset --hard
 git checkout master
 git pull origin master
 
-docker compose up -d --no-deps --build
-# docker compose stop
-# docker compose rm -f
-# docker compose up -d
+# docker compose up -d --no-deps --build
+docker compose stop
+docker compose rm -f
+docker compose up -d
